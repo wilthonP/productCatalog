2173a3acebe7d393d83e66f71d6c9cc8a8eb7822
Wilthon Peña <wilthon-pena95@hotmail.com>
2018-11-13 20:44:17 -0500

    Nuevo CRUD Localidad y Forma de pago
    Nuevo CRUD Localidad y Forma de pago

--- src/main/java/com/ts/productCatalog/repository/ImpuestoValorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ts.productCatalog.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.ts.productCatalog.entity.ImpuestoValor;
-
-public interface ImpuestoValorRepository extends MongoRepository<ImpuestoValor, String>{
-
-}
--- src/main/java/com/ts/productCatalog/repository/ItemServicioValorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ts.productCatalog.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.ts.productCatalog.entity.ItemServicioValor;
-
-public interface ItemServicioValorRepository extends MongoRepository<ItemServicioValor, String>{
-
-}
